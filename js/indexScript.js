80fd0faba8339e0a1eb80a5f847c0ad8a5395d91
PaulLinden <112555968+PaulLinden@users.noreply.github.com>
2023-02-14 16:40:42 +0100

    Added feats
    delat upp javascript och html.

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,16 +1,29 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+
+let button = document.querySelector('.dropbtn');
+
+//Eventlistner that opens menu when clicking on button
+button.addEventListener("click", (event) => {
+  console.log('open');
+  document.getElementById("myDropdown").classList.toggle("show");
+
+});
+
+
+window.onclick = function(event) {
   
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  if (!event.target.matches('.dropbtn')) {
+    console.log('close');
+    
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+      
+    for (i = 0; i < dropdowns.length; i++) {
+        
+      let openDropdown = dropdowns[i];
+      
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
