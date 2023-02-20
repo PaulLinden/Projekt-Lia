5ee0fec83dc7b57db26e0985283f5c1bed40cd39
PaulLinden <112555968+PaulLinden@users.noreply.github.com>
2023-02-20 07:38:26 +0100

    Menu Bugfix

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,29 +1,14 @@
-
-let button = document.querySelector('.dropbtn');
-
-//Eventlistner that opens menu when clicking on button
-button.addEventListener("click", (event) => {
-  console.log('open');
-  document.getElementById("myDropdown").classList.toggle("show");
-
-});
-
-
 window.onclick = function(event) {
+  console.log(event.target);
   
-  if (!event.target.matches('.dropbtn')) {
+  if (event.target.matches('.dropbtn') || event.target.matches('.hamburger')) {
+    console.log('open');
+    document.getElementById("myDropdown").style.display = 'block';
+     
+  }
+  else {
     console.log('close');
-    
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-      
-    for (i = 0; i < dropdowns.length; i++) {
-        
-      let openDropdown = dropdowns[i];
-      
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+
+    document.getElementById("myDropdown").style.display = 'none';
   }
 }
