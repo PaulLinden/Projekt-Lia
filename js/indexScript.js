04334e0bd00a94dfbe7a2dcec66c0434b2dc4b42
Tarek-Dali <112655234+Tarek-Dali@users.noreply.github.com>
2023-02-20 14:07:25 +0100

    Bug fix menu

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,14 +1,28 @@
-window.onclick = function(event) {
-  console.log(event.target);
-  
-  if (event.target.matches('.dropbtn') || event.target.matches('.hamburger')) {
-    console.log('open');
-    document.getElementById("myDropdown").style.display = 'block';
-     
-  }
-  else {
-    console.log('close');
 
-    document.getElementById("myDropdown").style.display = 'none';
+let button = document.querySelector('.dropbtn');
+
+menuStatus = 0;
+
+//Eventlistner that opens menu when clicking on button
+button.addEventListener("click", (event) => {
+  if(menuStatus == 0){
+    console.log('open');
+    document.getElementById("myDropdown").classList.toggle("show");
+    menuStatus++;
+  }else{
+    console.log("close");
+    document.getElementById('myDropdown').classList.remove('show');
+    menuStatus = 0;
+  }
+
+});
+
+
+window.onclick = function (event) {
+
+  if (!event.target.matches('.dropbtn') && menuStatus > 0) {
+    console.log("close");
+    document.getElementById('myDropdown').classList.remove('show');
+    menuStatus = 0;
   }
 }
